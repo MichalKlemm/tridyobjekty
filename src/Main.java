7a0b14b0bb2debccd43129def28b4714fc5ebe5b
MichalKlemm <Klemmmichal@lgmail.com>
2024-01-25 22:00:10 +0100

    Lekce 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,56 @@
+import com.engeto.ja.computers.Employee;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Employee employee1 =
+                new Employee("Karel Dvořák",
+                LocalDate.of( 1998, 1, 13),
+                BigDecimal.ZERO, 321);
+            // vytvoření objektu
+        Employee employee2 = new Employee("Jana Malá",
+                LocalDate.of(2005, 12, 8));
+        Employee employee3 = new Employee("Lukáš Čermák",
+                LocalDate.of(1989, 11, 8));
+
+        // seznamy
+        List<Employee> employeeList = new ArrayList<>();
+        employeeList.add(employee1);
+        employeeList.add(employee2);
+        employeeList.add(employee3);
+        System.out.println(" Počet zaměstnanců: " + employeeList.size());
+        for (int i = 1; i <= 10; i++){
+            employeeList.add(new Employee( "Zaměstnanec" ,+i, LocalDate.of(1990,1, i)));
+        }
+        System.out.println("Počet zaměstnanců: " + employeeList.size());
+        System.out.println("Jméno prvního zaměstnance: " + employeeList.get(0).getName());
+        System.out.println("Jméno druhého zaměstnance: " + employeeList.get(1).getName());
+
+
+        for (Employee: employeeList){
+            System.out.println(employee.getName() + "Sídlí v kanceláři " + employee.getOfficeNo());
+
+        }
+        employeeList.clear();
+        System.out.println("Počet zaměstnanců: " + employeeList.size());
+
+        // Takto NE (porušuje zapouzdření)
+        //   employee1.name = "Karel";
+
+        employee1.setName("Karel"); // volám metodu
+        System.out.println(employee1.getName());
+        // když dám ve třídě Employee private, tak mi getName nepojede
+        // protože to nebude dostupné pro tuto třídu
+
+        employee1.setAccountState(BigDecimal.valueOf(2));
+        System.out.println(
+            employee1.canBuy(BigDecimal.valueOf(2), 5));
+
+
+
     }
 }
--- src/com/engeto/ja/computers/Employee.java
+++ src/com/engeto/ja/computers/Employee.java
@@ -1,2 +1,81 @@
-package com.engeto.ja.computers;public class Employee {
+package com.engeto.ja.computers;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Employee {  //PascalCase - první slovo Velké písmeno a každé další s velým písmenem
+
+    private String name; //modifikátor přístupu private - princip zapouzdření (encapsulation)
+
+    private LocalDate birthdate;
+
+    private BigDecimal accountState; //camelCase - první malé, druhé velké písmeno
+
+    private int officeNo;
+
+    // Konstruktor
+
+
+    public Employee(String name, LocalDate birthdate, BigDecimal accountState, int officeNo) {
+        this.name = name;
+        this.birthdate = birthdate;
+        this.accountState = accountState;
+        this.officeNo = officeNo;
+    }
+
+    public Employee(String name, LocalDate birthdate){
+        this(name, birthdate, BigDecimal.ZERO, 115);
+    }
+
+
+    public boolean canBuy(BigDecimal pricePerPiece, int pieces){
+        BigDecimal totalPrice = pricePerPiece.multiply(BigDecimal.valueOf(pieces));
+        return accountState.compareTo(totalPrice) >= 0;
+    }
+
+    //region Přístupové metody
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) { // v závorce - doplňující parametry
+        this.name = name;               // více parametrú odděluji čárkami
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(LocalDate birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public BigDecimal getAccountstate() {
+        return accountState;
+    }
+
+    public void setAccountState(BigDecimal accountstate) {
+        this.accountState = accountstate;
+    }
+
+    public int getOfficeNo() {
+        return officeNo;
+    }
+
+    public void setOfficeNo(int officeNo) {
+        this.officeNo = officeNo;
+    }
+    //endregion
+
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", birthdate=" + birthdate +
+                ", accountState=" + accountState +
+                ", officeNo=" + officeNo +
+                '}';
+    }
 }
